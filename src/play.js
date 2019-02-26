c6ba06ff432af6e45c242d838916d3bcfb273cec
aamir soni <ll17a26s@leeds.ac.uk>
2019-02-26 00:04:00 +0000

    minor positioning changed made

--- src/play.js
+++ src/play.js
@@ -6,7 +6,8 @@ class PlayState extends Phaser.Scene {
     preload(){}
     create() {
         this.add.image(400,300,'court');
-        this.paddle1 = this.physics.add.sprite(100,200,'paddle');
+        this.paddle1 = this.physics.add.sprite(100,250,'paddle');
+        this.paddle2 = this.physics.add.sprite(700,250,'paddle');
         this.physics.add.sprite(200,300,'ball');
     }
     update() {}
